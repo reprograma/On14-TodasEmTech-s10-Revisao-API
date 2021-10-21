31a5f0289dbcacf9f782fb77c220a9347c5971c1
Érika de Freitas <erikafreitas47@gmail.com>
2021-10-21 00:06:31 -0300

    insere verbo post de erka

---
+++ para_o_lar/src/app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const routes = require("./routes/estabelecimentoRoutes");
+
+app.use(cors());
+app.use(express.json());
+
+app.use(routes);
+
+module.exports = app;
