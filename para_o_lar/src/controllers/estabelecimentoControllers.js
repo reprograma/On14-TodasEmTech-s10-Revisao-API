71f052002ff2f0ac574d1abe438ae78d8a787f42
Victoria Paiva <victoriardspaiva@recife.pe.senac.br>
2021-10-21 16:13:04 -0300

    primeiras rotas

--- para_o_lar/src/controllers/estabelecimentoControllers.js
+++ para_o_lar/src/controllers/estabelecimentoControllers.js
@@ -1,10 +1,21 @@
-const model = require("../models/estabelicimentos.json")
+const models = require("../models/estabelicimentos.json")
 
 const getAll = (req, res) => {
-    res.status(200).send(model)
+    res.status(200).send(models)
+}
+
+const getId = (req, res) => {
+    const IdSolicitado = req.params.id
+    const found = models.find(estabeleciamento => estabeleciamento.id == IdSolicitado)
+    
+    if(found == undefined){
+        res.status(404).send({message: "Estabelecimento não encontrado"})
+    }
+    res.status(200).send(found)
 }
 
 
 module.exports = {
-    getAll 
+    getAll,
+    getId
 }
--- para_o_lar/src/routers/estabelecimentoRoutes.js
+++ para_o_lar/src/routers/estabelecimentoRoutes.js
@@ -3,6 +3,7 @@ const { route } = require("../app")
 const controller = require("../controllers/estabelecimentoControllers")
 const router = express.Router()
 
-route.length("/todos", controller.getAll)
+route.get("/todos", controller.getAll)
+router.get("/:id", controller.getId)
 
 module.exports = router
