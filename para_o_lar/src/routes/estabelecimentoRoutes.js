62d40e5598cfcfd13306b165f8f26dbddb1c7b13
Érika de Freitas <erikafreitas47@gmail.com>
2021-10-22 02:36:28 -0300

    insere POST de erika

--- para_o_lar/src/routes/estabelecimentoRoutes.js
+++ para_o_lar/src/routes/estabelecimentoRoutes.js
@@ -3,10 +3,9 @@ const controller = require("../controllers/estabelecimentoController");
 const router = express.Router();
 
 router.get("/todos", controller.getAll);
+
 router.get("/:id", controller.getId);
 
 router.post("/criar", controller.criarEstabelecimento);
-
-
 
 module.exports = router;
