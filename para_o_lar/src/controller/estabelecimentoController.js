77fd133e7f5f4f842364a05a1c90a3bf21d53fdd
Edilainesds <edilaine45@yahoo.com.br>
2021-10-27 20:46:28 -0300

    entrega do  projeto feito em aula

---
+++ para_o_lar/src/controller/estabelecimentoController.js
@@ -0,0 +1,166 @@
+const models = require("../models/estabelecimentos.json");
+
+const getAll = ( req, res) => {
+
+    const {pagamento , bairro , delivery } = req.query
+
+    let filtrados = models 
+    // filtro por pagamento
+
+    if(pagamento){
+        filtrados = filtrados.filter(estabelecimento => {
+            return estabelecimento.pagamento.includes(pagamento)
+        }
+
+        )
+}
+
+    // filtro por bairro
+
+    if (bairro) {
+        filtrados = filtrados.filter(estabelecimento =>{
+            return estabelecimento.bairro == bairro
+        })
+    }
+
+    // filtro por delivery
+
+    if(delivery){
+        filtrados = filtrados.filter(estabelecimento =>{
+            return estabelecimento.delivery == (delivery == "true" ? true : false)
+        }
+
+        )
+    }
+    res.status(200).send(filtrados)
+
+}
+
+const getId = (req, res) =>{
+
+    const IdSolicitado = req.params.id
+    const { id } = req.params
+
+    const found = models.find(estabelecimento => estabelecimento.id == IdSolicitado)
+    if(found == undefined){
+        res.status(404).send({message: "estabelecimento nao encontrado"})
+    }
+    
+    res.status(200).send(found)
+
+}
+    const cadastrar = (req, res) =>{
+         let body = req.body
+        
+         const novoEstabelecimento = {
+             id: (models.length)+1,
+             likes:body.likes,
+             nome:body.nome,
+             categoria:body.categoria,
+             endereco:body.endereco,
+             numero:body.numero,
+             Bairro:body.Bairro,
+             cidade:body.cidade,
+             telefone:body.telefone,
+             pagamento:body.pagamento,
+             delivery:body.delivery,
+         }
+
+           if(!body.nome || !body.pagamento || !body.cidade){
+               return res.status(400).send({mensagem: "o campo nome é obrigatorio"})
+           }
+           if(body.nome.length > 10){
+               return res.status(400).send({mensagem: "voce ultrapassou o limite de 10 caracteres"})
+           }
+
+          models.push(novoEstabelecimento)
+
+          res.status(201).send(novoEstabelecimento)
+    }
+
+        const like = (req, res) => {
+            const id = req.params.id
+
+            const found = models.find(estabelecimento => estabelecimento.id == id)
+
+            if(found == undefined){
+                res.status(404).send({message: "estabelecimento nao encontrado"})
+            }
+            
+            found.likes += 1
+            res.status(200).send(found)
+     
+    }
+
+    
+
+        const deslike = (req, res) => {
+            const id = req.params.id
+
+            const found = models.find(estabelecimento => estabelecimento.id == id)
+
+            if(found == undefined){
+                res.status(404).send({message: "estabelecimento nao encontrado"})
+            }
+            
+            found.likes -= 1
+            res.status(200).send(found)
+     
+    }
+
+        const removerEstabelecimento  = (req, res) => {
+            const id = req.params.id 
+            const found = models.find(estabelecimento => estabelecimento.id == id)
+
+            if(found == undefined){
+                res.status(404).send({message: "estabelecimento nao encontrado"})
+
+            }
+            const index = models.indexOf(found)
+        }
+
+        const atualizacao = (req, res) => {
+            const id = req.params.id 
+            const found = models.find(estabelecimento => estabelecimento.id == idrequest)
+
+        
+
+            if(found == undefined){
+                res.status(404).send({message: "Estabelecimento nao encontrado"})
+
+            }
+            
+
+          const { nome, categoria, endereco, numero, bairro, cidade, telefone, pagamento, delivery} = req.body
+
+
+          found.nome = nome || found.nome
+          found.categoria = categoria || found.categoria
+          found.endereco = endereco || found.endereco
+          found.bairro = bairro || found.bairro
+          found.cidade = cidade || found.cidade
+          found.telefone = telefone || telefone.nome
+          found.pagamento = pagamento || found.pagamento
+          found.delivery = delivery || found.delivery
+
+          
+          
+          res.status(200).send(found)
+
+        }
+
+
+
+    module.exports = {
+
+    getAll ,
+    getId,
+    cadastrar,
+    like,
+    deslike,
+    removerEstabelecimento,
+    atualizacao
+    
+    
+}
+
