f382f60501461f032911ce0bd870d84cfde83301
daniele <danivilarez@hotmail.com>
2021-10-27 21:53:03 -0300

    ATUALIZAR

--- para_o_lar/src/controller/estabelecimentosController.js
+++ para_o_lar/src/controller/estabelecimentosController.js
@@ -31,13 +31,13 @@ const getAll = (request, response)=>{
     const idSolicitado= request.params.id
     const found = models.find(estabelecimento => estabelecimento.id == idSolicitado)
     
-
     if(found == undefined){
-        res.status(404).send({mensagem:"Estabelecimento não escontrado"})
+        response.status(404).send({message: 'Estabelecimento não encontrado'})
     }
-    response.status(200).send(found)
 
-}
+   response.status(200).send(found)
+
+  }
    
    const cadastrar = (request,response) =>{
      let body = request.body
@@ -54,9 +54,12 @@ const getAll = (request, response)=>{
         "delivery" :body.delivery
      }
 
-     console.log(cadastrarEstabelecimento);
-    models.push(cadastrarEstabelecimento)
-    response.status(201).send(
+     if(!body.nome){
+         return response.status(400).send({message: "O campo nome é obrigatorio"})
+     }
+
+     models.push(cadastrarEstabelecimento)
+     response.status(201).send(
         [
             {
                 "mensagem " : "estabelecimento cadastrado com sucesso",
@@ -66,9 +69,84 @@ const getAll = (request, response)=>{
     )
    }
    
+const like = (request,response)=>{ 
+    const {id}= request.params //const id =request.params.id
+    const found = models.find(estabelecimento => estabelecimento.id == id)
+ 
+    if(found == undefined){
+        response.status(404).send({message: 'Estabelecimento não encontrado'})
+    }
+
+    found.likes += 1
+
+    response.status(200).send(found)
+
+}
+const deslike = (request,response)=>{ 
+    const {id}= request.params //const id =request.params.id
+    const found = models.find(estabelecimento => estabelecimento.id == id)
+ 
+    if(found == undefined){
+        response.status(404).send({message: 'Estabelecimento não encontrado'})
+    }
+
+    found.likes -= 1
+
+    response.status(200).send(found)
+
+}
+const removeEstabelecimento = (request,response)=>{ 
+    const {id}= request.params //const id =request.params.id
+    const found = models.find(estabelecimento => estabelecimento.id == id)
+ 
+    if(found == undefined){
+        response.status(404).send({message: 'Estabelecimento não encontrado'})
+    }
+    const index = models.indexOf(found)
+    models.splice(index, 1)
+    //response.sendStatus(204)
+    response.status(200).send({message: 'Estabelecimento deletado'})
+
+}
+const atualizacao = (request,response)=>{ 
+   const idRequest = request.params.id
+   const bodyRequest = request.body
+   const found = models.find(estabelecimento => estabelecimento.id == idRequest)
+    bodyRequest.id = idRequest
+
+    Object.keys(found).forEach((informacao)=>{
+        if(bodyRequest[informacao] == undefined){
+            found[informacao] = found[informacao]
+        } else {
+            found[informacao] = bodyRequest[informacao]
+        }
+    })
+
+
+    if(found == undefined){
+        response.status(404).send({message: 'Estabelecimento não encontrado'})
+    }
+
+
+  // const { nome, categoria, endereco, bairro, cidade, telefone, pagamento, delivery } = request.body
+   //found.nome = nome || found.nome
+  // found.categoria = categoria || found.categoria
+  // found.endereco = endereco || found.endereco
+  // found.bairro = bairro || found.bairro
+  // found.cidade = cidade || found.cidade
+  // found.telefone = telefone || found.telefone
+  // found.pagamento = pagamento || found.pagamento
+  // found.delivery = delivery || found.delivery
+ 
+   response.status(200).send(found)
+}
 
 module.exports= {
     getId,
     getAll,
-    cadastrar
+    cadastrar,
+    like,
+    deslike,
+    removeEstabelecimento,
+    atualizacao
 }
--- para_o_lar/src/router/estabelecimentosRouter.js
+++ para_o_lar/src/router/estabelecimentosRouter.js
@@ -6,8 +6,11 @@ const router = express.Router();
 
 
 router.get('/todos', controller.getAll)
-router.get('/:id ',controller.getId)
-router.post("/criar",controller.cadastrar)
-
+router.get('/:id',controller.getId)
+router.post('/criar',controller.cadastrar)
+router.patch('/:id/like', controller.like)
+router.patch('/:id/deslike', controller.deslike)
+router.delete('/:id/remove',controller.removeEstabelecimento)
+router.put('/:id/atualiza', controller.atualizacao)
 
 module.exports = router
