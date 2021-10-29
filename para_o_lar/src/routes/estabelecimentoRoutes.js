ac7c3e6e2ff48d5ad3f51c39f90b77d7d2fe6711
Ananda Souto <88149291+ansndsc@users.noreply.github.com>
2021-10-29 16:25:08 -0300

    Adiciona routes feitos em aula 23/10

--- para_o_lar/src/routes/estabelecimentoRoutes.js
+++ para_o_lar/src/routes/estabelecimentoRoutes.js
@@ -4,10 +4,18 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/:todos", controller.getAll);
+router.get("/todos", controller.getAll);
 
-router.get("/:id", controller.getById);
+router.get("/:id", controller.getId);
 
 router.post("/cadastro", controller.cadastro);
+
+router.patch("/:id/like", controller.updateLike);
+
+router.patch("/:id/deslike", controller.updateDeslike);
+
+router.delete("/:id/delete", controller.deleteEstabelecimento);
+
+router.put("/:id/atualiza", controller.atualizacao);
 
 module.exports = router;
