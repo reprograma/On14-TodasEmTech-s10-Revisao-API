6f6c6c9bb56edff0de0c30033d58b277cf71b2f6
Edlaine Pontes <edlaine.psico@gmail.com>
2021-11-03 12:46:59 -0300

    atualizacao aula

---
+++ para_o_lar/src/routes/estabelecimentoRoutes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const controller = require('../controller/estabelecimentoController');
+const router = express.Router();
+
+router.get('/todos', controller.getAll)
+router.get('/:id', controller.getId)
+router.post('/cadastro', controller.cadastrar)
+router.patch('/:id/like', controller.like)
+router.patch('/:id/deslike', controller.deslike)
+router.delete('/:id/remove', controller.removeEstabelecimento)
+router.put('/:id/atualiza', controller.atualizacao)
+
+module.exports = router
